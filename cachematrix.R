4b3eaafdf756d158cde02de3b056d4236f2e5bb0
csull9 <csull9@gmail.com>
2017-01-19 15:46:53 -0800

    added cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
+Fork the GitHub repository containing the stub R files at https://github.com/rdpeng/ProgrammingAssignment2 to create a copy under your own account.
+Clone your forked GitHub repository to your computer so that you can edit the files locally on your own machine.
+Edit the R file contained in the git repository and place your solution in that file (please do not rename the file).
+Commit your completed R file into YOUR git repository and push your git branch to the GitHub repository under your account.
+Submit to Coursera the URL to your GitHub repository that contains the completed R code for the assignment.## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Takes a matrix and returns a list to set value of the matrix, get its value
+## set the value of its inverse, and get the value of its inverse 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y){
+		x <<- y
+		inv <<- NULL
+	}
+	get <- function() x 
+	setinverse <- function(solve) inv <<- solve
+	getinverse <- function() inv
+	list(set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Takes the above function and computes matrix of inverse, saying if it has been
+## calculated from a cache 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setinverse(m)
+	m
 }
